00fc4a42461e999438207f120c6a5ebe660f8d78
markpark65 <jhmark65@naver.com>
2026-04-29 09:24:38 +0900

    Fix WireSpark audio crash, wbp_loading

--- Source/ApocalypseDelivery/Private/ChasingEnemy.cpp
+++ Source/ApocalypseDelivery/Private/ChasingEnemy.cpp
@@ -139,7 +139,6 @@ void AChasingEnemy::PlayOverlapEffects() {
 }
 
 void AChasingEnemy::ApplyEffect_Implementation(class ADrone* Drone) {
-	WarningAudioComp->Stop();
 	if (IsValid(WarningAudioComp)) {
 		WarningAudioComp->Stop();
 	}
